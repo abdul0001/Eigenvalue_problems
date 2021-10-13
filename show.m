9466b2425d6a460113e5d1bcb9ec5364e9808cb4
Abdul Halim <32664853+abdul0001@users.noreply.github.com>
2021-10-13 12:21:21 +0200

    Delete show.m

--- show.m
+++
@@ -1,5 +0,0 @@
-function show(coordinates,elements,uh)
-%subplot(1,2,1)
-trisurf(elements,coordinates(:,1),coordinates(:,2),uh','facecolor','interp')
-% subplot(1,2,2)
-% trisurf(elements,coordinates(:,1),coordinates(:,2),u','facecolor','interp')
