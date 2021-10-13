6b31f7017ebf8d5846297c50a3bde8228b9c799f
Abdul Halim <32664853+abdul0001@users.noreply.github.com>
2021-10-13 12:19:25 +0200

    Delete P2shapes.m

--- P2shapes.m
+++
@@ -1,6 +0,0 @@
-function [S,dSdr,dSds]=P2shapes(r,s)
-S=[1-3*r-3*s+2*r^2+4*r*s+2*s^2;2*r^2-r;2*s^2-s;
-    4*r-4*r^2-4*r*s;4*r*s;4*s-4*r*s-4*s^2];
-dSdr=[-3+4*r+4*s;4*r-1;0;4-8*r-4*s;4*s;-4*s];
-dSds=[-3+4*r+4*s;0;4*s-1;-4*r;4*r;4-4*r-8*s];
-end
