622d981fcf938e272bdc8ba3de2f2ac3c6cabead
Abdul Halim <32664853+abdul0001@users.noreply.github.com>
2021-10-13 12:18:59 +0200

    Delete Isopmap.m

--- Isopmap.m
+++
@@ -1,7 +0,0 @@
-function [S,dSdx,dSdy,detJ] = Isopmap(x,y,r,s,shapefcn)
-[S,dSdr,dSds]=shapefcn(r,s);
-j11=dot(dSdr,x); j12=dot(dSdr,y);
-j21=dot(dSds,x); j22=dot(dSds,y);
-detJ=j11*j22-j12*j21;
-dSdx=( j22*dSdr-j12*dSds)/detJ; dSdy=(-j21*dSdr+j11*dSds)/detJ;
-end
